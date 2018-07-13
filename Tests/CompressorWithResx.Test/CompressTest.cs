1ced06d4760730b5a72606923af6f9e196996ad2
Martin Karing <nitram@illarion.org>
2018-07-14 00:12:07 +0200

    Added UnitTest to cover compressor problem
    Added a UnitTest that covers the crash of the compressor that occured
    in case the assembly contained EmbeddedResources (such as ResX files).

---
+++ Tests/CompressorWithResx.Test/CompressTest.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+using Confuser.Core;
+using Confuser.Core.Project;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace CompressorWithResx.Test {
+	public class CompressTest {
+		private readonly ITestOutputHelper outputHelper;
+
+		public CompressTest(ITestOutputHelper outputHelper) => 
+			this.outputHelper = outputHelper ?? throw new ArgumentNullException(nameof(outputHelper));
+
+		[Fact]
+		public async Task CompressAndExecuteTest() {
+			var baseDir = Environment.CurrentDirectory;
+			var outputFile = Path.Combine(baseDir, "testtmp", "CompressorWithResx.exe");
+			ClearOutput(outputFile);
+			var proj = new ConfuserProject {
+				BaseDirectory = baseDir,
+				OutputDirectory = Path.Combine(baseDir, "testtmp"),
+				Packer = new SettingItem<Packer>("compressor")
+			};
+			proj.Add(new ProjectModule() { Path = Path.Combine(baseDir, "CompressorWithResx.exe") });
+
+
+			var parameters = new ConfuserParameters {
+				Project = proj,
+				Logger = new XunitLogger(outputHelper)
+			};
+
+			await ConfuserEngine.Run(parameters);
+
+			Assert.True(File.Exists(outputFile));
+
+			var info = new ProcessStartInfo(outputFile) {
+				RedirectStandardOutput = true,
+				UseShellExecute = false
+			};
+			using (var process = Process.Start(info)) {
+				var stdout = process.StandardOutput;
+				Assert.Equal("START", await stdout.ReadLineAsync());
+				Assert.Equal("Test", await stdout.ReadLineAsync());
+				Assert.Equal("END", await stdout.ReadLineAsync());
+				Assert.Empty(await stdout.ReadToEndAsync());
+				Assert.True(process.HasExited);
+				Assert.Equal(42, process.ExitCode);
+			}
+
+			ClearOutput(outputFile);
+		}
+
+		private static void ClearOutput(string outputFile) {
+			try {
+				if (File.Exists(outputFile)) {
+					File.Delete(outputFile);
+				}
+			}
+			catch (UnauthorizedAccessException) { }
+			var debugSymbols = Path.ChangeExtension(outputFile, "pdb");
+			try {
+				if (File.Exists(debugSymbols)) {
+					File.Delete(debugSymbols);
+				}
+			}
+			catch (UnauthorizedAccessException) { }
+		}
+	}
+}
---
+++ Tests/CompressorWithResx.Test/XUnitLogger.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Confuser.Core;
+using Xunit.Abstractions;
+
+namespace CompressorWithResx.Test {
+	internal sealed class XunitLogger : ILogger {
+		private readonly ITestOutputHelper outputHelper;
+
+		internal XunitLogger(ITestOutputHelper outputHelper) => 
+			this.outputHelper = outputHelper ?? throw new ArgumentNullException(nameof(outputHelper));
+
+		void ILogger.Debug(string msg) => 
+			outputHelper.WriteLine("[DEBUG] " + msg);
+
+		void ILogger.DebugFormat(string format, params object[] args) =>
+			outputHelper.WriteLine("[DEBUG] " + format, args);
+
+		void ILogger.EndProgress() { }
+
+		void ILogger.Error(string msg) => 
+			throw new Exception(msg);
+
+		void ILogger.ErrorException(string msg, Exception ex) => 
+			throw new Exception(msg, ex);
+
+		void ILogger.ErrorFormat(string format, params object[] args) => 
+			throw new Exception(string.Format(format, args));
+
+		void ILogger.Finish(bool successful) =>
+			outputHelper.WriteLine("[DONE]");
+
+		void ILogger.Info(string msg) =>
+			outputHelper.WriteLine("[INFO] " + msg);
+
+		void ILogger.InfoFormat(string format, params object[] args) =>
+			outputHelper.WriteLine("[INFO] " + format, args);
+
+		void ILogger.Progress(int progress, int overall) { }
+
+		void ILogger.Warn(string msg) =>
+			outputHelper.WriteLine("[WARN] " + msg);
+
+		void ILogger.WarnException(string msg, Exception ex) =>
+			outputHelper.WriteLine("[WARN] " + msg + Environment.NewLine + ex.ToString());
+
+		void ILogger.WarnFormat(string format, params object[] args) =>
+			outputHelper.WriteLine("[WARN] " + format, args);
+	}
+}
