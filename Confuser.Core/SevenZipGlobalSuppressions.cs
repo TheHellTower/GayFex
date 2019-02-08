1749d9771fc71dbf1bb7922d9f3d904d584ad462
Martin Karing <karing.martin@gmail.com>
2019-02-08 23:26:01 +0100

    Suppress 7z Warnings in Core Runtime

--- Confuser.Core/SevenZipGlobalSuppressions.cs
+++ Confuser.Core/SevenZipGlobalSuppressions.cs
@@ -1,10 +1,4 @@
-﻿
-// This file is used by Code Analysis to maintain SuppressMessage 
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given 
-// a specific target and scoped to a namespace, type, member, etc.
-
-using System.Diagnostics.CodeAnalysis;
+﻿using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage("Microsoft.Design", "CA1032:ImplementStandardExceptionConstructors", Scope = "type", Target = "~T:SevenZip.InvalidParamException")]
 [assembly: SuppressMessage("Microsoft.Design", "CA1032:ImplementStandardExceptionConstructors", Scope = "type", Target = "~T:SevenZip.DataErrorException")]
@@ -13,6 +7,11 @@ using System.Diagnostics.CodeAnalysis;
 [assembly: SuppressMessage("Microsoft.Design", "CA1051:DoNotDeclareVisibleInstanceFields", Scope = "type", Target = "~T:SevenZip.Compression.LZ.InWindow")]
 [assembly: SuppressMessage("Microsoft.Design", "CA1051:DoNotDeclareVisibleInstanceFields", Scope = "type", Target = "~T:SevenZip.Compression.LZ.OutWindow")]
 
+[assembly: SuppressMessage("Microsoft.Globalization", "CA1304:SpecifyCultureInfo", Scope = "type", Target = "~T:SevenZip.Compression.LZMA.Encoder")]
+
 [assembly: SuppressMessage("Microsoft.Usage", "CA1801:ReviewUnusedParameters", Scope = "type", Target = "~T:SevenZip.Compression.LZMA.Encoder")]
 [assembly: SuppressMessage("Microsoft.Usage", "CA1801:ReviewUnusedParameters", Scope = "type", Target = "~T:SevenZip.Compression.RangeCoder.Decoder")]
 
+[assembly: SuppressMessage("Microsoft.Performance", "CA1810:InitializeReferenceTypeStaticFieldsInline", Scope = "type", Target = "~T:SevenZip.CRC")]
+[assembly: SuppressMessage("Microsoft.Performance", "CA1822:MarkMembersAsStatic", Scope = "type", Target = "~T:SevenZip.Compression.LZ.BinTree")]
+[assembly: SuppressMessage("Microsoft.Performance", "CA1823:AvoidUnusedPrivateFields", Scope = "type", Target = "~T:SevenZip.Compression.LZMA.Encoder")]
