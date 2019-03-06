23f5e61a3c979514df016eb6700d40026519ce38
Martin Karing <karing.martin@gmail.com>
2019-03-06 10:45:41 +0100

    refs #25 Created unit test for issue

---
+++ Tests/NeutralResx/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NeutralResx {
+	public class Program {
+		internal static int Main(string[] args) {
+			Console.WriteLine("START");
+			Console.WriteLine(Properties.Resources.TestString);
+			Console.WriteLine("END");
+			return 42;
+		}
+	}
+}
