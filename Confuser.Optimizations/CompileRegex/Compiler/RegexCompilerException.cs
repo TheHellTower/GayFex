6f806320fdad3b89bbc90959ade7ac9348589797
Martin Karing <karing.martin@gmail.com>
2019-03-24 18:44:16 +0100

    Suppressed irrelevant compiler warning

--- Confuser.Optimizations/CompileRegex/Compiler/RegexCompilerException.cs
+++ Confuser.Optimizations/CompileRegex/Compiler/RegexCompilerException.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Runtime.Serialization;
 using Confuser.Core;
 
 namespace Confuser.Optimizations.CompileRegex.Compiler {
 	[Serializable]
-	internal class RegexCompilerException : ConfuserException {
+	[SuppressMessage("Microsoft.Design", "CA1032:ImplementStandardExceptionConstructors", 
+		Justification = "Additional constructors are currently not required and will be added as needed.")]
+	internal sealed class RegexCompilerException : ConfuserException {
 		private RegexCompilerException(string message, Exception innerException) : base(message, innerException) {
 		}
 
@@ -12,7 +15,7 @@ namespace Confuser.Optimizations.CompileRegex.Compiler {
 			this("Compiling the expression \"" + compileDef.Pattern + "\" failed.", innerException) {
 		}
 
-		protected RegexCompilerException(SerializationInfo info, StreamingContext context) : base(info, context) {
+		private RegexCompilerException(SerializationInfo info, StreamingContext context) : base(info, context) {
 		}
 	}
 }
