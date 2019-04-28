efb98dbc2aba367ef105944fcd06e8f1b180ba8f
Martin Karing <karing.martin@gmail.com>
2019-04-28 11:16:51 +0200

    refs #39 Added analyzer for reflection
    The renaming protection now has an additional service for the usage of reflection.

---
+++ Confuser.Renamer/Analyzers/ReflectionAnalyzer.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Confuser.Core;
+using Confuser.Core.Services;
+using dnlib.DotNet;
+using dnlib.DotNet.Emit;
+
+namespace Confuser.Renamer.Analyzers {
+	/// <summary>
+	/// This analyzer is looking for calls to the reflection API and blocks methods from being renamed if required.
+	/// </summary>
+	public sealed class ReflectionAnalyzer : IRenamer {
+		void IRenamer.Analyze(ConfuserContext context, INameService service, ProtectionParameters parameters, IDnlibDef def) {
+			if (!(def is MethodDef method) || !method.HasBody) return;
+
+			Analyze(service, context.Registry.GetService<ITraceService>(), context.Modules.Cast<ModuleDef>().ToArray(), method);
+		}
+
+		public void Analyze(INameService nameService, ITraceService traceService, IReadOnlyList<ModuleDef> moduleDefs, MethodDef method) {
+			MethodTrace methodTrace = null;
+			MethodTrace GetMethodTrace() {
+				if (methodTrace == null)
+					methodTrace = traceService.Trace(method);
+				return methodTrace;
+			}
+
+			foreach (var instr in method.Body.Instructions) {
+				if (instr.OpCode.Code == Code.Call && instr.Operand is IMethodDefOrRef calledMethod) {
+					if (calledMethod.DeclaringType.FullName == "System.Type") {
+						Func<TypeDef, IEnumerable<IMemberDef>> getMember = null;
+						if (calledMethod.Name == nameof(Type.GetMethod))
+							getMember = t => t.Methods;
+						else if (calledMethod.Name == nameof(Type.GetField))
+							getMember = t => t.Fields;
+						else if (calledMethod.Name == nameof(Type.GetProperty))
+							getMember = t => t.Properties;
+						else if (calledMethod.Name == nameof(Type.GetEvent))
+							getMember = t => t.Events;
+						else if (calledMethod.Name == nameof(Type.GetMember))
+							getMember = t => Enumerable.Empty<IMemberDef>().Concat(t.Methods).Concat(t.Fields).Concat(t.Properties).Concat(t.Events);
+
+						if (getMember != null) {
+							var trace = GetMethodTrace();
+							var arguments = trace.TraceArguments(instr);
+							if (arguments.Length >= 2) {
+								var types = GetReferencedTypes(method.Body.Instructions[arguments[0]], method, trace);
+								var names = GetReferencedNames(method.Body.Instructions[arguments[1]]);
+
+								if (!types.Any())
+									types = moduleDefs.SelectMany(m => m.GetTypes()).ToArray();
+
+								foreach (var possibleMethod in types.SelectMany(getMember).Where(m => names.Contains(m.Name))) {
+									nameService.SetCanRename(possibleMethod, false);
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+
+		/// <summary>
+		/// This method is used to determine the types that are load onto the stack at the referenced instruction.
+		/// In case the method is unable to determine all the types reliable, it will return a empty list.
+		/// </summary>
+		private static IReadOnlyList<TypeDef> GetReferencedTypes(Instruction instruction, MethodDef method, MethodTrace trace) {
+			if (instruction.OpCode.Code == Code.Call && instruction.Operand is IMethodDefOrRef calledMethod) {
+				if (calledMethod.DeclaringType.FullName == "System.Type" && calledMethod.Name == "GetTypeFromHandle") {
+					var arguments = trace.TraceArguments(instruction);
+					if (arguments.Length == 1) {
+						var ldTokenInstr = method.Body.Instructions[arguments[0]];
+						if (ldTokenInstr.OpCode.Code == Code.Ldtoken && ldTokenInstr.Operand is TypeDef refTypeDef) {
+							return new List<TypeDef>() { refTypeDef };
+						}
+					}
+				}
+			}
+
+			return new List<TypeDef>();
+		}
+
+		private static IReadOnlyList<UTF8String> GetReferencedNames(Instruction instruction) {
+			if (instruction.OpCode.Code == Code.Ldstr && instruction.Operand is string str) {
+				return new List<UTF8String>() { str };
+			}
+
+			return new List<UTF8String>();
+		}
+
+		void IRenamer.PostRename(ConfuserContext context, INameService service, ProtectionParameters parameters, IDnlibDef def) { }
+
+		void IRenamer.PreRename(ConfuserContext context, INameService service, ProtectionParameters parameters, IDnlibDef def) { }
+	}
+}
