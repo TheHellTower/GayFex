b21529c9029d7da26e275646f56a917a5a668947
Martin Karing <karing.martin@gmail.com>
2019-09-20 00:07:39 +0200

    refs #87 Restructed VTableAnalyzer for unit tests

--- Confuser.Renamer/Analyzers/VTableAnalyzer.cs
+++ Confuser.Renamer/Analyzers/VTableAnalyzer.cs
@@ -7,65 +7,71 @@ using Confuser.Renamer.References;
 using dnlib.DotNet;
 
 namespace Confuser.Renamer.Analyzers {
-	internal class VTableAnalyzer : IRenamer {
-		public void Analyze(ConfuserContext context, INameService service, ProtectionParameters parameters, IDnlibDef def) {
-			VTable vTbl;
-
-			if (def is TypeDef) {
-				var type = (TypeDef)def;
-				if (type.IsInterface)
-					return;
-
-				vTbl = service.GetVTables()[type];
-				foreach (var ifaceVTbl in vTbl.InterfaceSlots.Values) {
-					foreach (var slot in ifaceVTbl) {
-						if (slot.Overrides == null)
-							continue;
-						Debug.Assert(slot.Overrides.MethodDef.DeclaringType.IsInterface);
-						// A method in base type can implements an interface method for a
-						// derived type. If the base type/interface is not in our control, we should
-						// not rename the methods.
-						bool baseUnderCtrl = context.Modules.Contains(slot.MethodDef.DeclaringType.Module as ModuleDefMD);
-						bool ifaceUnderCtrl = context.Modules.Contains(slot.Overrides.MethodDef.DeclaringType.Module as ModuleDefMD);
-						if ((!baseUnderCtrl && ifaceUnderCtrl) || !service.CanRename(slot.MethodDef)) {
-							service.SetCanRename(slot.Overrides.MethodDef, false);
-						}
-						else if (baseUnderCtrl && !ifaceUnderCtrl || !service.CanRename(slot.Overrides.MethodDef)) {
-							service.SetCanRename(slot.MethodDef, false);
-						}
-					}
-				}
+	public class VTableAnalyzer : IRenamer {
+		void IRenamer.Analyze(ConfuserContext context, INameService service, ProtectionParameters parameters, IDnlibDef def) {
+			switch (def) {
+				case TypeDef typeDef:
+					Analyze(service, context.Modules, typeDef);
+					break;
+				case MethodDef methodDef:
+					Analyze(service, context.Modules, methodDef);
+					break;
 			}
-			else if (def is MethodDef) {
-				var method = (MethodDef)def;
-				if (!method.IsVirtual)
-					return;
+		}
 
-				vTbl = service.GetVTables()[method.DeclaringType];
-				VTableSignature sig = VTableSignature.FromMethod(method);
-				var slots = vTbl.FindSlots(method);
+		public static void Analyze(INameService service, ICollection<ModuleDefMD> modules, TypeDef type) {
+			if (type.IsInterface)
+				return;
 
-				if (!method.IsAbstract) {
-					foreach (var slot in slots) {
-						if (slot.Overrides == null)
-							continue;
-
-						SetupOverwriteReferences(context, service, slot, method.Module);
-					}
-				}
-				else {
-					foreach (var slot in slots) {
-						if (slot.Overrides == null)
-							continue;
-						service.SetCanRename(method, false);
+			var vTbl = service.GetVTables()[type];
+			foreach (var ifaceVTbl in vTbl.InterfaceSlots.Values) {
+				foreach (var slot in ifaceVTbl) {
+					if (slot.Overrides == null)
+						continue;
+					Debug.Assert(slot.Overrides.MethodDef.DeclaringType.IsInterface);
+					// A method in base type can implements an interface method for a
+					// derived type. If the base type/interface is not in our control, we should
+					// not rename the methods.
+					bool baseUnderCtrl = modules.Contains(slot.MethodDef.DeclaringType.Module as ModuleDefMD);
+					bool ifaceUnderCtrl = modules.Contains(slot.Overrides.MethodDef.DeclaringType.Module as ModuleDefMD);
+					if ((!baseUnderCtrl && ifaceUnderCtrl) || !service.CanRename(slot.MethodDef)) {
 						service.SetCanRename(slot.Overrides.MethodDef, false);
+					}
+					else if (baseUnderCtrl && !ifaceUnderCtrl || !service.CanRename(slot.Overrides.MethodDef)) {
+						service.SetCanRename(slot.MethodDef, false);
 					}
 				}
 			}
 		}
 
-		private static void AddImportReference(ConfuserContext context, INameService service, ModuleDef module, MethodDef method, MemberRef methodRef) {
-			if (method.Module != module && context.Modules.Contains((ModuleDefMD)module)) {
+		public static void Analyze(INameService service, ICollection<ModuleDefMD> modules, MethodDef method) {
+			if (!method.IsVirtual)
+				return;
+
+			var vTbl = service.GetVTables()[method.DeclaringType];
+			VTableSignature sig = VTableSignature.FromMethod(method);
+			var slots = vTbl.FindSlots(method);
+
+			if (!method.IsAbstract) {
+				foreach (var slot in slots) {
+					if (slot.Overrides == null)
+						continue;
+
+					SetupOverwriteReferences(service, modules, slot, method.Module);
+				}
+			}
+			else {
+				foreach (var slot in slots) {
+					if (slot.Overrides == null)
+						continue;
+					service.SetCanRename(method, false);
+					service.SetCanRename(slot.Overrides.MethodDef, false);
+				}
+			}
+		}
+
+		private static void AddImportReference(INameService service, ICollection<ModuleDefMD> modules,  ModuleDef module, MethodDef method, MemberRef methodRef) {
+			if (method.Module != module && modules.Contains((ModuleDefMD)module)) {
 				var declType = (TypeRef)methodRef.DeclaringType.ScopeType;
 				service.AddReference(method.DeclaringType, new TypeRefReference(declType, method.DeclaringType));
 				service.AddReference(method, new MemberRefReference(methodRef, method));
@@ -74,34 +80,34 @@ namespace Confuser.Renamer.Analyzers {
 				typeRefs.AddRange(methodRef.MethodSig.RetType.FindTypeRefs());
 				typeRefs.AddRange(methodRef.DeclaringType.ToTypeSig().FindTypeRefs());
 				foreach (var typeRef in typeRefs) {
-					SetupTypeReference(context, service, module, typeRef);
+					SetupTypeReference(service, modules, module, typeRef);
 				}
 			}
 		}
 
-		private static void SetupTypeReference(ConfuserContext context, INameService service, ModuleDef module, ITypeDefOrRef typeDefOrRef) {
+		private static void SetupTypeReference(INameService service, ICollection<ModuleDefMD> modules, ModuleDef module, ITypeDefOrRef typeDefOrRef) {
 			if (!(typeDefOrRef is TypeRef typeRef)) return;
 
 			var def = typeRef.ResolveTypeDefThrow();
-			if (def.Module != module && context.Modules.Contains((ModuleDefMD)def.Module))
+			if (def.Module != module && modules.Contains((ModuleDefMD)def.Module))
 				service.AddReference(def, new TypeRefReference(typeRef, def));
 		}
 
-		private static GenericInstSig SetupSignatureReferences(ConfuserContext context, INameService service, ModuleDef module, GenericInstSig typeSig) {
-			var genericType = SetupSignatureReferences(context, service, module, typeSig.GenericType);
-			var genericArguments = typeSig.GenericArguments.Select(a => SetupSignatureReferences(context, service, module, a)).ToList();
+		private static GenericInstSig SetupSignatureReferences(INameService service, ICollection<ModuleDefMD> modules, ModuleDef module, GenericInstSig typeSig) {
+			var genericType = SetupSignatureReferences(service, modules, module, typeSig.GenericType);
+			var genericArguments = typeSig.GenericArguments.Select(a => SetupSignatureReferences(service, modules, module, a)).ToList();
 			return new GenericInstSig(genericType, genericArguments);
 		}
 
-		private static T SetupSignatureReferences<T>(ConfuserContext context, INameService service, ModuleDef module, T typeSig) where T : TypeSig {
+		private static T SetupSignatureReferences<T>(INameService service, ICollection<ModuleDefMD> modules,  ModuleDef module, T typeSig) where T : TypeSig {
 			var asTypeRef = typeSig.TryGetTypeRef();
 			if (asTypeRef != null) {
-				SetupTypeReference(context, service, module, asTypeRef);
+				SetupTypeReference(service, modules, module, asTypeRef);
 			}
 			return typeSig;
 		}
 
-		private static void SetupOverwriteReferences(ConfuserContext context, INameService service, VTableSlot slot, ModuleDef module) {
+		private static void SetupOverwriteReferences(INameService service, ICollection<ModuleDefMD> modules, VTableSlot slot, ModuleDef module) {
 			var methodDef = slot.MethodDef;
 			var baseSlot = slot.Overrides;
 			var baseMethodDef = baseSlot.MethodDef;
@@ -114,7 +120,7 @@ namespace Confuser.Renamer.Analyzers {
 
 			IMethodDefOrRef target;
 			if (baseSlot.MethodDefDeclType is GenericInstSig declType) {
-				var signature = SetupSignatureReferences(context, service, module, declType);
+				var signature = SetupSignatureReferences(service, modules, module, declType);
 				MemberRef targetRef = new MemberRefUser(module, baseMethodDef.Name, baseMethodDef.MethodSig, signature.ToTypeDefOrRef());
 				targetRef = importer.Import(targetRef);
 				service.AddReference(baseMethodDef, new MemberRefReference(targetRef, baseMethodDef));
@@ -132,7 +138,7 @@ namespace Confuser.Renamer.Analyzers {
 
 			target.MethodSig = importer.Import(target.MethodSig);
 			if (target is MemberRef methodRef)
-				AddImportReference(context, service, module, baseMethodDef, methodRef);
+				AddImportReference(service, modules, module, baseMethodDef, methodRef);
 
 			if (methodDef.Overrides.Any(impl => IsMatchingOverride(impl, target)))
 				return;
