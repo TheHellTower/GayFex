1d009c58c33fb797f455b51f999434e4d84e690b
Martin Karing <karing.martin@gmail.com>
2020-01-12 11:47:04 +0100

    refs #109 Constants DynamicMode: Code Cleanup

--- Confuser.Protections/Constants/DynamicMode.cs
+++ Confuser.Protections/Constants/DynamicMode.cs
@@ -1,30 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using Confuser.Core.Helpers;
 using Confuser.Core.Services;
 using Confuser.DynCipher;
-using Confuser.DynCipher.AST;
 using Confuser.DynCipher.Generation;
 using Confuser.Helpers;
 using dnlib.DotNet;
 using dnlib.DotNet.Emit;
-using Microsoft.Extensions.DependencyInjection;
 
 namespace Confuser.Protections.Constants {
-	internal class DynamicMode : IEncodeMode {
-		Action<uint[], uint[]> encryptFunc;
+	internal partial class DynamicMode : IEncodeMode {
+		private Action<uint[], uint[]> _encryptFunc;
 
 		CryptProcessor IEncodeMode.EmitDecrypt(CEContext ctx) {
-			StatementBlock encrypt, decrypt;
-			ctx.DynCipher.GenerateCipherPair(ctx.Random, out encrypt, out decrypt);
+			ctx.DynCipher.GenerateCipherPair(ctx.Random, out var encrypt, out var decrypt);
 
 			var dmCodeGen = new DMCodeGen(typeof(void), new[] {
 				Tuple.Create("{BUFFER}", typeof(uint[])),
 				Tuple.Create("{KEY}", typeof(uint[]))
 			});
 			dmCodeGen.GenerateCIL(encrypt);
-			encryptFunc = dmCodeGen.Compile<Action<uint[], uint[]>>();
+			_encryptFunc = dmCodeGen.Compile<Action<uint[], uint[]>>();
 
 			return (module, method, block, key) => {
 				var ret = new List<Instruction>();
@@ -38,7 +34,7 @@ namespace Confuser.Protections.Constants {
 		public uint[] Encrypt(uint[] data, int offset, uint[] key) {
 			var ret = new uint[key.Length];
 			Buffer.BlockCopy(data, offset * sizeof(uint), ret, 0, key.Length * sizeof(uint));
-			encryptFunc(ret, key);
+			_encryptFunc(ret, key);
 			return ret;
 		}
 
@@ -46,18 +42,18 @@ namespace Confuser.Protections.Constants {
 			uint k1 = ctx.Random.NextUInt32() | 1;
 			uint k2 = ctx.Random.NextUInt32();
 
-			IReadOnlyList<Instruction> processor(ModuleDef module, MethodDef method, IReadOnlyList<Instruction> arg) {
-				var repl = new List<Instruction>(arg.Count + 4);
-				repl.AddRange(arg);
-				repl.Add(Instruction.Create(OpCodes.Ldc_I4, (int)MathsUtils.ModInv(k1)));
-				repl.Add(Instruction.Create(OpCodes.Mul));
-				repl.Add(Instruction.Create(OpCodes.Ldc_I4, (int)k2));
-				repl.Add(Instruction.Create(OpCodes.Xor));
-				return repl.ToArray();
+			IReadOnlyList<Instruction> Processor(ModuleDef module, MethodDef method, IReadOnlyList<Instruction> arg) {
+				var replacement = new List<Instruction>(arg.Count + 4);
+				replacement.AddRange(arg);
+				replacement.Add(Instruction.Create(OpCodes.Ldc_I4, (int)MathsUtils.ModInv(k1)));
+				replacement.Add(Instruction.Create(OpCodes.Mul));
+				replacement.Add(Instruction.Create(OpCodes.Ldc_I4, (int)k2));
+				replacement.Add(Instruction.Create(OpCodes.Xor));
+				return replacement.ToArray();
 			}
 
 			;
-			return (processor, Tuple.Create(k1, k2));
+			return (Processor, Tuple.Create(k1, k2));
 		}
 
 		public uint Encode(object data, CEContext ctx, uint id) {
@@ -65,25 +61,6 @@ namespace Confuser.Protections.Constants {
 			uint ret = (id ^ key.Item2) * key.Item1;
 			Debug.Assert(((ret * MathsUtils.ModInv(key.Item1)) ^ key.Item2) == id);
 			return ret;
-		}
-
-		class CodeGen : CILCodeGen {
-			readonly Local block;
-			readonly Local key;
-
-			public CodeGen(Local block, Local key, ModuleDef module, MethodDef init, IList<Instruction> instrs)
-				: base(module, init, instrs) {
-				this.block = block;
-				this.key = key;
-			}
-
-			protected override Local Var(Variable var) {
-				if (var.Name == "{BUFFER}")
-					return block;
-				if (var.Name == "{KEY}")
-					return key;
-				return base.Var(var);
-			}
 		}
 	}
 }
---
+++ Confuser.Protections/Constants/DynamicMode_CodeGen.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Confuser.DynCipher.AST;
+using Confuser.DynCipher.Generation;
+using dnlib.DotNet;
+using dnlib.DotNet.Emit;
+
+namespace Confuser.Protections.Constants {
+	internal partial class DynamicMode {
+		private sealed class CodeGen : CILCodeGen {
+			private readonly Local _block;
+			private readonly Local _key;
+
+			public CodeGen(Local block, Local key, ModuleDef module, MethodDef init, IList<Instruction> instructions)
+				: base(module, init, instructions) {
+				_block = block;
+				_key = key;
+			}
+
+			protected override Local Var(Variable var) {
+				switch (var.Name) {
+					case "{BUFFER}":
+						return _block;
+					case "{KEY}":
+						return _key;
+					default:
+						return base.Var(var);
+				}
+			}
+		}
+	}
+}
