efaa1f23bd79da0b4fbe88b3cdf419c9ac1c3eec
ElektroKill <skullmaster0103@gmail.com>
2020-06-19 19:19:04 +0200

    TypeScrambler: Fixed compatibility issue with Clr20 assemblies.
    Unlike Clr40 assemblies they require all fields to use MeberRefs and not only static fields

--- Confuser.Protections/TypeScrambler/Scrambler/Rewriter/Instructions/FieldDefInstructionRewriter.cs
+++ Confuser.Protections/TypeScrambler/Scrambler/Rewriter/Instructions/FieldDefInstructionRewriter.cs
@@ -5,13 +5,22 @@ using dnlib.DotNet.Emit;
 namespace Confuser.Protections.TypeScramble.Scrambler.Rewriter.Instructions {
 	internal sealed class FieldDefInstructionRewriter : InstructionRewriter<FieldDef> {
 		internal override void ProcessOperand(TypeService service, MethodDef method, IList<Instruction> body, ref int index, FieldDef operand) {
-			if (body[index].OpCode != OpCodes.Ldsfld && body[index].OpCode != OpCodes.Ldsflda && body[index].OpCode != OpCodes.Stsfld) 
-				return;
+			if (method.Module.IsClr40) {
+				if (body[index].OpCode != OpCodes.Ldsfld && body[index].OpCode != OpCodes.Ldsflda && body[index].OpCode != OpCodes.Stsfld)
+					return;
 
-			var declType = service.GetItem(operand.DeclaringType);
-			if (declType?.IsScambled == true) {
-				body[index].Operand = new MemberRefUser(operand.Module, operand.Name, operand.FieldSig,
-					declType.CreateGenericTypeSig(service.GetItem(method.DeclaringType)).ToTypeDefOrRef());
+				var declType = service.GetItem(operand.DeclaringType);
+				if (declType?.IsScambled == true) {
+					body[index].Operand = new MemberRefUser(operand.Module, operand.Name, operand.FieldSig,
+						declType.CreateGenericTypeSig(service.GetItem(method.DeclaringType)).ToTypeDefOrRef());
+				}
+			}
+			else {
+				var declType = service.GetItem(operand.DeclaringType);
+				if (declType?.IsScambled == true) {
+					body[index].Operand = new MemberRefUser(operand.Module, operand.Name, operand.FieldSig,
+						declType.CreateGenericTypeSig(service.GetItem(method.DeclaringType)).ToTypeDefOrRef());
+				}
 			}
 		}
 	}
