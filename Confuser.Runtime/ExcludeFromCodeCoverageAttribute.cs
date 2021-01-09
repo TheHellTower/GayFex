755f18812daa161b3fd4f2131ff7c34d52dbc07c
Martin Karing <karing.martin@gmail.com>
2021-01-09 18:31:09 +0100

    Build Confuser Runtime for .NET Framework 2.0
    The runtime for ConfuserEx is now build for .NET 2.0 to ensure that it is compatible with the old framework version.

---
+++ Confuser.Runtime/ExcludeFromCodeCoverageAttribute.cs
@@ -0,0 +1,7 @@
+﻿#if NETFRAMEWORK && (NET20 || NET35)
+// ReSharper disable once CheckNamespace
+namespace System.Diagnostics.CodeAnalysis {
+	internal sealed class ExcludeFromCodeCoverageAttribute : Attribute {
+	}
+}
+#endif
