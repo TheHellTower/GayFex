96322bce5e9195ef4c0edcbd6c1400d78388da81
Martin Karing <karing.martin@gmail.com>
2021-01-10 22:23:50 +0100

    Adding unit tests for CLR protection issues

---
+++ Tests/244_ClrProtection.Test/ProtectClrAssemblyTest.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Threading.Tasks;
+using Confuser.Core;
+using Confuser.Core.Project;
+using Confuser.UnitTest;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace ClrProtection.Test {
+	public class ProtectClrAssemblyTest : TestBase {
+		public ProtectClrAssemblyTest(ITestOutputHelper outputHelper) : base(outputHelper) { }
+
+		[Fact]
+		[Trait("Category", "Protection")]
+		[Trait("Protection", "anti tamper")]
+		[Trait("Issue", "https://github.com/mkaring/ConfuserEx/issues/244")]
+		public Task AntiTamperProtection() => Run(
+			"244_ClrProtection.exe",
+			Array.Empty<String>(),
+			new SettingItem<Protection>("anti tamper"),
+			$"_{nameof(AntiTamperProtection)}");
+
+		[Fact]
+		[Trait("Category", "Protection")]
+		[Trait("Protection", "resources")]
+		[Trait("Issue", "https://github.com/mkaring/ConfuserEx/issues/244")]
+		public Task ResourceProtection() => Run(
+			"244_ClrProtection.exe",
+			Array.Empty<String>(),
+			new SettingItem<Protection>("resources"),
+			$"_{nameof(ResourceProtection)}");
+
+		[Fact]
+		[Trait("Category", "Protection")]
+		[Trait("Protection", "typescramble")]
+		[Trait("Issue", "https://github.com/mkaring/ConfuserEx/issues/244")]
+		public Task TypeScrambleProtection() => Run(
+			"244_ClrProtection.exe",
+			Array.Empty<String>(),
+			new SettingItem<Protection>("typescramble"),
+			$"_{nameof(TypeScrambleProtection)}");
+	}
+}
