d97522f6edc95d0bc08d2be7bd0caac05015075e
Martin Karing <karing.martin@gmail.com>
2021-03-14 00:04:11 +0100

    Improved constant protection unit test

--- Tests/ConstantProtection.Test/ConstantProtectionTest.cs
+++ Tests/ConstantProtection.Test/ConstantProtectionTest.cs
@@ -7,68 +7,36 @@ using System.Threading.Tasks;
 using Confuser.Core;
 using Confuser.Core.Project;
 using Confuser.UnitTest;
-using Microsoft.Extensions.Logging;
 using Xunit;
 using Xunit.Abstractions;
 using System.Linq;
+using Confuser.Protections.Constants;
+using Confuser.Core.Services;
 
 namespace ConstantProtection.Test {
-	public sealed class ConstantProtectionTest {
-		private readonly ITestOutputHelper outputHelper;
+	public sealed class ConstantProtectionTest : TestBase {
+		public ConstantProtectionTest(ITestOutputHelper outputHelper) : base(outputHelper) { }
 
-		public ConstantProtectionTest(ITestOutputHelper outputHelper) =>
-			this.outputHelper = outputHelper ?? throw new ArgumentNullException(nameof(outputHelper));
+		public static TheoryData<ConstantProtectionTestCase> ProtectAndExecuteTestData { get; } = BuildProtectAndExecuteTestData();
 
 		[Theory]
 		[MemberData(nameof(ProtectAndExecuteTestData))]
 		[Trait("Category", "Protection")]
 		[Trait("Protection", "constants")]
-		public async Task ProtectAndExecuteTest(string framework, string modeKey, string compressor, bool cfgKey, string elementsKey) {
-			var key = Path.Combine(Environment.CurrentDirectory, "Confuser.Test.snk");
-			var baseDir = Path.Combine(Environment.CurrentDirectory, framework);
-			var outputDir = Path.Combine(baseDir, "testtmp_" + Guid.NewGuid().ToString());
+		public async Task ProtectAndExecuteTest(ConstantProtectionTestCase testData) {
+			var baseDir = Path.Combine(Environment.CurrentDirectory, testData.Framework);
 			var inputFile = Path.Combine(baseDir, "ConstantProtection.exe");
-			var outputFile = Path.Combine(outputDir, "ConstantProtection.exe");
-
-			Assert.True(File.Exists(key));
-			FileUtilities.ClearOutput(outputFile);
-			var proj = new ConfuserProject {
-				BaseDirectory = baseDir,
-				OutputDirectory = outputDir
-			};
-
-			proj.Rules.Add(new Rule() {
-				new SettingItem<IProtection>("constants") {
-					{ "mode", modeKey },
-					{ "compressor", compressor },
-					{ "cfg", cfgKey ? "true" : "false" },
-					{ "elements", elementsKey }
-				}
-			});
-
-			proj.Add(new ProjectModule() { Path = inputFile, SNKeyPath = key });
-
-			var parameters = new ConfuserParameters {
-				Project = proj,
-				ConfigureLogging = builder => builder.AddProvider(new XunitLogger(outputHelper))
-			};
 
 			var recordedResult = await ProcessUtilities.ExecuteTestApplication(inputFile, RecordOutput, outputHelper).ConfigureAwait(true);
 
-			await ConfuserEngine.Run(parameters).ConfigureAwait(true);
-
-			Assert.True(File.Exists(outputFile));
-			Assert.NotEqual(FileUtilities.ComputeFileChecksum(inputFile), FileUtilities.ComputeFileChecksum(outputFile));
-
-			var result = await ProcessUtilities.ExecuteTestApplication(outputFile, async stdout => {
-				await VerifyOutput(recordedResult.Result, stdout).ConfigureAwait(true);
-			}, outputHelper).ConfigureAwait(true);
-			Assert.Equal(42, result);
-
-			FileUtilities.ClearOutput(outputFile);
+			await Run(testData.Framework, 
+				"ConstantProtection.exe", 
+				recordedResult.Result, 
+				testData.Apply(new SettingItem<IProtection>("constants")),
+				outputDirSuffix: Guid.NewGuid().ToString());
 		}
 
-		private async Task<IReadOnlyList<string>> RecordOutput(StreamReader reader) {
+		private static async Task<string[]> RecordOutput(StreamReader reader) {
 			var result = new List<string>();
 
 			string line;
@@ -76,32 +44,40 @@ namespace ConstantProtection.Test {
 				result.Add(line);
 			}
 
-			return result;
+			Assert.Equal("START", result[0]);
+			Assert.Equal("END", result[^1]);
+
+			result.RemoveAt(0);
+			result.RemoveAt(result.Count - 1);
+
+			return result.ToArray();
 		}
 
-		private async Task VerifyOutput(IReadOnlyList<string> expected, StreamReader actual) {
-			foreach (var expectedResult in expected) {
-				Assert.Equal(expectedResult, await actual.ReadLineAsync().ConfigureAwait(true));
-			}
-		}
-
-		public static IEnumerable<object[]> ProtectAndExecuteTestData() =>
+		internal static TheoryData<ConstantProtectionTestCase> BuildProtectAndExecuteTestData() => (
 			from framework in new string[] { "net20", "net40", "net471" }
-			from mode in new string[] { "Normal", "Dynamic", "x86" }
-			from compressor in new string[] { "None", "Deflate", "Lzma", "Lz4" }
+			from mode in Enum.GetValues<Mode>()
+			from compressor in  Enum.GetValues<CompressionAlgorithm>()
 			from cfg in new bool[] { false, true }
-			from encodeStrings in new string[] { "", "S" }
-			from encodeNumbers in new string[] { "", "N" }
-			from encodePrimitives in GetPrimitivesOptions(encodeStrings, encodeNumbers)
-			from encodeInitializers in new string[] { "", "I" }
-			select new object[] { framework, mode, compressor, cfg, encodeStrings + encodeNumbers + encodePrimitives + encodeInitializers };
+			from encodeStrings in new EncodeElements[] { EncodeElements.None, EncodeElements.Strings }
+			from encodeNumbers in new EncodeElements[] { EncodeElements.None, EncodeElements.Numbers }
+			from encodePrimitives in GetPrimitivesOptions(encodeStrings | encodeNumbers)
+			from encodeInitializers in new EncodeElements[] { EncodeElements.None, EncodeElements.Initializers }
+			select new ConstantProtectionTestCase() with { 
+				Framework = framework,
+				Mode = mode,
+				Compressor = compressor,
+				ControlFlowGraph = cfg,
+				Elements = encodeStrings | encodeNumbers | encodePrimitives | encodeInitializers 
+			}).Aggregate(new TheoryData<ConstantProtectionTestCase>(), (td, tc) => { td.Add(tc); return td; });
 
-		private static string[] GetPrimitivesOptions(string encodeStrings, string encodeNumbers) {
-			string[] primitivesOptions;
-			if (string.IsNullOrEmpty(encodeStrings) && string.IsNullOrEmpty(encodeNumbers))
-				primitivesOptions = new string[] { "" };
+		private static EncodeElements[] GetPrimitivesOptions(EncodeElements encodeElements) {
+			EncodeElements[] primitivesOptions;
+
+			if ((encodeElements & (EncodeElements.Strings | EncodeElements.Numbers)) == 0)
+				primitivesOptions = new EncodeElements[] { EncodeElements.None };
 			else
-				primitivesOptions = new string[] { "", "P" };
+				primitivesOptions = new EncodeElements[] { EncodeElements.None, EncodeElements.Primitive };
+
 			return primitivesOptions;
 		}
 	}
---
+++ Tests/ConstantProtection.Test/ConstantProtectionTestCase.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using Confuser.Core;
+using Confuser.Core.Project;
+using Confuser.Core.Services;
+using Confuser.Protections.Constants;
+using Xunit.Abstractions;
+
+namespace ConstantProtection.Test {
+	public sealed record ConstantProtectionTestCase : IXunitSerializable {
+		internal static ConstantProtectionParameters Parameters { get; } = new ConstantProtectionParameters();
+
+		internal string Framework { get; set; }
+		internal Mode Mode { get; set; }
+		internal CompressionAlgorithm Compressor { get; set; }
+		internal bool ControlFlowGraph { get; set; }
+		internal EncodeElements Elements { get; set; }
+
+		public void Deserialize(IXunitSerializationInfo info) {
+			Framework = info.GetValue<string>(nameof(Framework));
+			Mode = Parameters.Mode.Deserialize(info.GetValue<string>(nameof(Mode)));
+			Compressor = Parameters.Compressor.Deserialize(info.GetValue<string>(nameof(Compressor)));
+			ControlFlowGraph = Parameters.ControlFlowGraphReplacement.Deserialize(info.GetValue<string>(nameof(ControlFlowGraph)));
+			Elements = Parameters.Elements.Deserialize(info.GetValue<string>(nameof(Elements)));
+		}
+
+		public void Serialize(IXunitSerializationInfo info) {
+			info.AddValue(nameof(Framework), Framework);
+			info.AddValue(nameof(Mode), Parameters.Mode.Serialize(Mode));
+			info.AddValue(nameof(Compressor), Parameters.Compressor.Serialize(Compressor));
+			info.AddValue(nameof(ControlFlowGraph), Parameters.ControlFlowGraphReplacement.Serialize(ControlFlowGraph));
+			info.AddValue(nameof(Elements), Parameters.Elements.Serialize(Elements));
+		}
+
+		internal SettingItem<IProtection> Apply(SettingItem<IProtection> settings) {
+			settings.Add(Parameters.Mode.Name, Parameters.Mode.Serialize(Mode));
+			settings.Add(Parameters.Compressor.Name, Parameters.Compressor.Serialize(Compressor));
+			settings.Add(Parameters.ControlFlowGraphReplacement.Name, Parameters.ControlFlowGraphReplacement.Serialize(ControlFlowGraph));
+			settings.Add(Parameters.Elements.Name, Parameters.Elements.Serialize(Elements));
+			return settings;
+		}
+
+		public override string ToString() =>
+			$"{Framework}, mode: {Mode}, compression: {Compressor}, cfg: {ControlFlowGraph}, elements: {Elements}";
+	}
+}
