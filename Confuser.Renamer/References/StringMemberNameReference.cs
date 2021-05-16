6345db263c5b9880a50ba0308fa0142d1a544146
Martin Karing <karing.martin@gmail.com>
2021-05-16 11:39:59 +0200

    Added support for C# CallSite Binder

---
+++ Confuser.Renamer/References/StringMemberNameReference.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+using Confuser.Core;
+using dnlib.DotNet;
+using dnlib.DotNet.Emit;
+
+namespace Confuser.Renamer.References {
+	public sealed class StringMemberNameReference : INameReference<IMemberDef> {
+		private readonly Instruction _reference;
+		private readonly IMemberDef _memberDef;
+		public bool ShouldCancelRename => false;
+
+		public StringMemberNameReference(Instruction reference, IMemberDef memberDef) {
+			_reference = reference;
+			_memberDef = memberDef;
+		}
+
+		/// <inheritdoc />
+		public bool DelayRenaming(INameService service) => false;
+
+		public bool UpdateNameReference(ConfuserContext context, INameService service) {
+			switch (_reference.Operand) {
+				case string strOp when string.Equals(strOp, _memberDef.Name, StringComparison.Ordinal):
+				case UTF8String utf8StrOp when UTF8String.Equals(utf8StrOp, _memberDef.Name):
+					return false;
+				default:
+					_reference.Operand = (string)_memberDef.Name;
+					return true;
+			}
+		}
+
+		public override string ToString() => ToString(null);
+
+		public string ToString(INameService nameService) {
+			var builder = new StringBuilder();
+			builder.Append("String Member Name Reference").Append("(");
+
+			builder.Append("Instruction").Append("(").AppendHashedIdentifier("Operand", _reference.Operand).Append(")");
+			builder.Append("; ");
+			builder.AppendReferencedDef(_memberDef, nameService);
+
+			builder.Append(")");
+
+			return builder.ToString();
+		}
+	}
+}
---
+++ Tests/345_RenameDynamicParameter/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace RenameDynamicParameter {
+	public class Program {
+		internal static int Main(string[] args) {
+			Console.WriteLine("START");
+			
+			SimpleTestClass.TestStatic();
+			SimpleTestClass.TestDynamic();
+
+			OverloadTestClass.TestString();
+			OverloadTestClass.TestInteger();
+
+			var overrideTest = new OverrideTestClass();
+			overrideTest.TestString();
+			overrideTest.TestInteger();
+
+			var fieldTest = new FieldTestClass();
+			fieldTest.TestDynamic();
+
+			ConstructorTestClass.TestString();
+			ConstructorTestClass.TestInteger();
+
+			Console.WriteLine("END");
+
+			return 42;
+		}
+	}
+}
