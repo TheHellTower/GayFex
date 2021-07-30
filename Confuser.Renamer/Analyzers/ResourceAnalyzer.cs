4720cbe2f6386cbad4f15ca1c318155aaacaaf35
Martin Karing <karing.martin@gmail.com>
2021-07-31 00:11:28 +0200

    Case insensitive handling of culture identifier
    The culture identifier of resources is not handled case sensitive anymore. Even if the case of the culture does not match, the renaming will be handled properly. The casing of the tag will be retained.
    
    Also added a test to cover this specific case.

--- Confuser.Renamer/Analyzers/ResourceAnalyzer.cs
+++ Confuser.Renamer/Analyzers/ResourceAnalyzer.cs
@@ -19,7 +19,7 @@ namespace Confuser.Renamer.Analyzers {
 			if (!string.IsNullOrEmpty(module.Assembly.Culture) &&
 				asmName.EndsWith(".resources")) {
 				// Satellite assembly
-				var satellitePattern = new Regex(string.Format("^(.*)\\.{0}\\.resources$", module.Assembly.Culture));
+				string satellitePattern = $"^(.*)\\.(?i:({module.Assembly.Culture}))\\.resources$";
 				string nameAsmName = asmName.Substring(0, asmName.Length - ".resources".Length);
 				ModuleDef mainModule = context.Modules.SingleOrDefault(mod => mod.Assembly.Name == nameAsmName);
 				if (mainModule == null) {
@@ -27,17 +27,19 @@ namespace Confuser.Renamer.Analyzers {
 					throw new ConfuserException(null);
 				}
 
-				string format = "{0}." + module.Assembly.Culture + ".resources";
 				foreach (Resource res in module.Resources) {
-					Match match = satellitePattern.Match(res.Name);
-					if (!match.Success)
-						continue;
+					Match match = Regex.Match(res.Name, satellitePattern);
+					if (!match.Success) continue;
 					string typeName = match.Groups[1].Value;
+					string culture = match.Groups[2].Value;
+
 					TypeDef type = mainModule.FindReflection(typeName);
 					if (type == null) {
 						context.Logger.WarnFormat(Resources.ResourceAnalyzer_Analyze_CouldNotFindResourceType, typeName);
 						continue;
 					}
+					string format = $"{{0}}.{culture}.resources";
+
 					service.ReduceRenameMode(type, RenameMode.Reflection);
 					service.AddReference(type, new ResourceReference(res, type, format));
 				}
---
+++ Tests/389_MixedCultureCasing.Test/MixedCultureCasingTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using Confuser.Core;
+using Confuser.Core.Project;
+using Confuser.UnitTest;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace MixedCultureCasing.Test
+{
+    public class MixedCultureCasingTest : TestBase {
+		public MixedCultureCasingTest(ITestOutputHelper outputHelper) : base(outputHelper) { }
+
+		[Fact]
+		[Trait("Category", "Protection")]
+		[Trait("Protection", "rename")]
+		[Trait("Issue", "https://github.com/mkaring/ConfuserEx/issues/389")]
+		public async Task MixedCultureCasing() =>
+			await Run(
+				new [] {
+					"389_MixedCultureCasing.exe",
+					@"de-DE\389_MixedCultureCasing.resources.dll"
+				},
+				new [] {
+					"Test 1 (neutral)",
+					"Test 1 (deutsch)",
+					"Test 2 (neutral)",
+					"Test 2 (deutsch)"
+				},
+				new SettingItem<Protection>("rename")
+			);
+	}
+}
