e81454db7e9f81e379048191fa3e281c01c9650e
Ivan Kochurkin <kvanttt@gmail.com>
2021-08-15 17:26:46 +0300

    Add BlockingReferences test that reveals #379

---
+++ Tests/BlockingReferences.Test/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Confuser.Core;
+using Confuser.Core.Project;
+using Confuser.UnitTest;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace BlockingReferences.Test {
+	public class BlockingReferencesTest : TestBase {
+		public BlockingReferencesTest(ITestOutputHelper outputHelper) : base(outputHelper) { }
+
+		[Fact]
+		[Trait("Category", "Protection")]
+		[Trait("Protection", "rename")]
+		[Trait("Issue", "https://github.com/mkaring/ConfuserEx/issues/379")]
+		public async Task BlockingReferences() =>
+			await Run(
+				new [] { "BlockingReferences.exe", "BlockingReferencesHelper.dll" },
+				new [] {
+					"",
+					"Implementation2",
+				},
+				new SettingItem<Protection>("rename") { ["renPublic"] = "true", ["mode"] = "decodable" }
+			);
+	}
+}
---
+++ Tests/BlockingReferences/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using BlockingReferencesHelper;
+
+namespace BlockingReferences {
+	public interface IBaseInterface {
+		string Method();
+	}
+
+	public class Implementation1 : BaseImplementation<string>, IBaseInterface {
+	}
+
+	public class Implementation2 : BaseImplementation<string>, IBaseInterface {
+		public override string Method() => "Implementation2";
+	}
+
+	public static class Program {
+		public static int Main() {
+			Console.WriteLine("START");
+			Console.WriteLine(new Implementation1().Method());
+			Console.WriteLine(new Implementation2().Method());
+			Console.WriteLine("END");
+
+			return 42;
+		}
+	}
+}
